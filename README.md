5f853b2e34a04783abe4b9ced45ec538670f5202
Angelo Zamora <sicosis3dfx@outlook.com>
2026-08-25 12:48:15 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ti3041-p2-c3-2026p-u1
 Repositorio de clase a clase Backend INACAP Renca 2026 P
+
+python -m venv .venv
--- bienvenida/models.py
+++ bienvenida/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Producto(models.Model):
     nombre= models.CharField(max_length=64)
+    descripcion= models.CharField(max_length=128, blank=True,null=True)
     precio= models.IntegerField(default=0)
-    descripcion= models.CharField(max_length=128, blank=True, null=True)
     stock= models.IntegerField(default=0)
